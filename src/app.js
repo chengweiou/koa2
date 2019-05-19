b95cf9882ac6c0e33b947636b61b2b8a8a398a34
chengweiou <chengwei.ou.dora@gmail.com>
2019-05-19 01:43:20 -0700

    un comment db, change loginaccount handler by no login

--- src/app.js
+++ src/app.js
@@ -15,5 +15,5 @@ const server = app.listen(config.get('server.port'))
 
 require('./core/log/realtime').init(app.listen(config.get('server.log.port')))
 
-// const mongoose = require('mongoose')
-// mongoose.connect(config.get('db.host'), {useNewUrlParser: true})
+const mongoose = require('mongoose')
+mongoose.connect(config.get('db.host'), {useNewUrlParser: true})
