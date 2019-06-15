d7601cdf02c386888860e0ef9ee59f90da857ed2
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 16:57:25 -0700

    uat path

--- uat.sh
+++ uat.sh
@@ -1,7 +1,7 @@
-cp -r src ~/Desktop/docker/wormhole-node/
-cp package.json ~/Desktop/docker/wormhole-node/
-cp -r config ~/Desktop/docker/wormhole-node/
-cd ~/Desktop/docker/wormhole-node
+cp -r src ~/Desktop/docker/universe/wormhole-node/
+cp package.json ~/Desktop/docker/universe/wormhole-node/
+cp -r config ~/Desktop/docker/universe/wormhole-node/
+cd ~/Desktop/docker/universe/wormhole-node
 docker stop wormhole-node
 docker stop wormhole
-docker run --rm -it -d --name wormhole-node -p 60000:8906 --network net -v /Users/chengweiou/Desktop/docker/wormhole-node:/proj/ -w /proj/ node yarn serve:uat
+docker run --rm -it -d --name wormhole-node -p 60000:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/wormhole-node:/proj/ -w /proj/ node yarn serve:uat
