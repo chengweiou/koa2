b21c4ccea73694dd8c51fe04a248f1b1efe4a999
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 16:57:54 -0700

    uat path

--- uat.sh
+++ uat.sh
@@ -1,6 +1,6 @@
-cp -r src ~/Desktop/docker/xxx-node/
-cp package.json ~/Desktop/docker/xxx-node/
-cp -r config ~/Desktop/docker/xxx-node/
-cd ~/Desktop/docker/xxx-node
+cp -r src ~/Desktop/docker/universe/xxx-node/
+cp package.json ~/Desktop/docker/universe/xxx-node/
+cp -r config ~/Desktop/docker/universe/xxx-node/
+cd ~/Desktop/docker/universe/xxx-node
 docker stop xxx-node
-docker run --rm -it -d --name xxx-node -p 60001:8906 --network net -v /Users/chengweiou/Desktop/docker/xxx-node:/proj/ -w /proj/ node yarn serve:uat
+docker run --rm -it -d --name xxx-node -p 60001:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/xxx-node:/proj/ -w /proj/ node yarn serve:uat
