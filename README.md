1b5be529baff6644059e0684d4b3eaeba7ab346c
chengweiou <chengwei.ou.dora@gmail.com>
2019-08-25 14:03:28 -0700

    add zip node_modules in uat

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ first time:
 chmod +x uat.sh
 ./uat.sh
 ```
+each time node_modules update, add blow to uat.sh
+```
+zip -qr node_modules.zip node_modules
+cp node_modules.zip ~/Desktop/docker/universe/wormhole/
+rm -rf node_modules.zip
+cd ~/Desktop/docker/universe/wormhole
+unzip -o node_modules.zip
+cd ~/Desktop/docker/universe/wormhole
+```
--- uat.sh
+++ uat.sh
@@ -1,7 +1,14 @@
-cp -r src ~/Desktop/docker/universe/wormhole-node/
-cp package.json ~/Desktop/docker/universe/wormhole-node/
-cp -r config ~/Desktop/docker/universe/wormhole-node/
-cd ~/Desktop/docker/universe/wormhole-node
-docker stop wormhole-node
+cp -r src ~/Desktop/docker/universe/wormhole/
+cp package.json ~/Desktop/docker/universe/wormhole/
+cp -r config ~/Desktop/docker/universe/wormhole/
+
+# zip -qr node_modules.zip node_modules
+# cp node_modules.zip ~/Desktop/docker/universe/wormhole/
+# rm -rf node_modules.zip
+# cd ~/Desktop/docker/universe/wormhole
+# unzip -o node_modules.zip
+# cd ~/Desktop/docker/universe/wormhole
+
+cd ~/Desktop/docker/universe/wormhole
 docker stop wormhole
-docker run --rm -it -d --name wormhole-node -p 60000:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/wormhole-node:/proj/ -w /proj/ node yarn serve:uat
+docker run --rm -it -d --name wormhole -p 60000:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/wormhole:/proj/ -w /proj/ node yarn serve:uat
