750b9193dd108d20c3087f7f97232c49f8ebe819
chengweiou <chengwei.ou.dora@gmail.com>
2021-06-15 15:26:19 -0700

    backup

--- README.md
+++ README.md
@@ -23,3 +23,10 @@ cd ~/Desktop/docker/universe/wormhole
 unzip -o node_modules.zip
 cd ~/Desktop/docker/universe/wormhole
 ```
+
+##### 单独启动容器
+```
+docker network net
+docker run --rm -it --name mongodb -p 27017:27017 --network net -v ~/Desktop/docker/mongodb/data:/data/db -d mongo
+docker run --rm -it -d --name wormhole -p 60000:8906 --network net -v ~/Desktop/docker/universe/wormhole:/proj/ -w /proj/ node yarn serve:uat
+```
--- uat.sh
+++ uat.sh
@@ -9,6 +9,9 @@ cp -r config ~/Desktop/docker/universe/wormhole/
 # unzip -o node_modules.zip
 # cd ~/Desktop/docker/universe/wormhole
 
+cp docker-compose.yml ~/Desktop/docker/universe/wormhole/docker-compose.yml
 cd ~/Desktop/docker/universe/wormhole
-docker stop wormhole
-docker run --rm -it -d --name wormhole -p 60000:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/wormhole:/proj/ -w /proj/ node yarn serve:uat
+
+docker compose down
+docker compose rm -f
+docker compose up -d
